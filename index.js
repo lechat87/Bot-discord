0c1bf836ab0f06ca335382271829617f25addb97
lechat87 <148372845+lechat87@users.noreply.github.com>
2024-02-13 08:21:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const client = new Discord.Client();
 const PREFIX = '+';
 const TOKEN = 'MTIwMjI4MzI5NTcwMzA0NDE2OA.GNTcyK.CUvk5g-P_LT3TEfWX9vy9ICGFWsDdMX0d73e1E';
 
+intents = discord.Intents.default()
+intents.bans = True
+intents.messages = True
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.content.startswith('+ban'):
+        # Vérifier si l'utilisateur a la permission de bannir des membres
+        if not message.author.guild_permissions.ban_members:
+            return await message.channel.send('Vous n\'avez pas la permission de bannir des membres.')
+
+        await message.channel.send('Qui voulez-vous bannir ?')
+
+        def check(msg):
+            return msg.author == message.author and msg.channel == message.channel
+
+        try:
+            member_msg = await bot.wait_for('message', check=check, timeout=60)
+            member = message.guild.get_member(int(member_msg.content.replace('<@!', '').replace('>', '')))
+            if not member:
+                return await message.channel.send('Membre introuvable.')
+
+            await message.channel.send('Pour quelle raison ? (envoyée en mp)')
+
+            reason_msg = await bot.wait_for('message', check=check, timeout=60)
+
+            await member.ban(reason=reason_msg.content)
+            await message.channel.send(f'{member.mention} a été banni pour {reason_msg.content}.')
+        except asyncio.TimeoutError:
+            await message.channel.send('Temps écoulé. Veuillez réessayer.')
+
+    await bot.process_commands(message)
+
 client.on('message', message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(PREFIX)) return;
